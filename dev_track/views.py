6111d364465f048bd400027c2e93114c1a2cba9e
vernondcole <vernon.cole@ehealthnigeria.org>
2014-08-12 19:01:13 +0100

    spell HttpResponse correctly

--- dev_track/views.py
+++ dev_track/views.py
@@ -1,8 +1,8 @@
 from __future__ import print_function, unicode_literals
 
-from django.http import HttpRespense
+from django.http import HttpResponse
 #from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpRespense('Hello, world ... the dev-demo is running.')
+    return HttpResponse('Hello, world ... the dev-demo is running.')
