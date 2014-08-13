b9d1b157b8a7b2fd47495d8d991818f8f8f59e10
vernondcole <vernon.cole@ehealthnigeria.org>
2014-08-13 20:25:09 +0100

    put the home page on /index, too..

--- demo/urls.py
+++ demo/urls.py
@@ -7,7 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'dev_track.views.home', name='home'),
     url(r'^index.*', 'dev_track.views.home', name='home'),
-   # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
