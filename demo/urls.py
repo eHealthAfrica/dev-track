f131f1ac7c8cebf8dac25d13f229695419c01bfb
vernondcole <vernon.cole@ehealthnigeria.org>
2014-08-15 20:15:22 +0100

    use html template

--- demo/urls.py
+++ demo/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import TemplateView
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'dev_track.views.home', name='home'),
-    url(r'^index.*', 'dev_track.views.home', name='home'),
-
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^admin/', include(admin.site.urls)),
 )
--- dev_track/views.py
+++ dev_track/views.py
@@ -1,8 +1,4 @@
-from __future__ import print_function, unicode_literals
-
-from django.http import HttpResponse
 #from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return HttpResponse('Hello, world ... the dev-demo is running.')
+
